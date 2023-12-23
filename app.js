8f6749e35653d11382fa06ee92c23df4de1a2667
Lovebirdflying <gomitoguns@gmail.com>
2023-12-23 18:47:45 +0800

    atlas

--- app.js
+++ app.js
@@ -73,8 +73,7 @@ app.use((req, res) => {
 });
 
 
-
-mongoose.connect("mongodb://127.0.0.1:27017/hic")
+mongoose.connect(process.env.MONGO)
 .then((done)=>{
     console.log("db connection is successful")
 
